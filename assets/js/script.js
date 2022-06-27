0b6f7f55abfcd7ba1daccb700925091238aeb6d3
sadafsafi22 <safisadaf96@gmail.com>
2022-06-27 05:27:24 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,23 +11,31 @@ document.addEventListener("DOMContentLoaded",function(){
                 alert('You clicked the Submit button');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`you have clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
 });
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
 
     // Create tow random Numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    //console.log(num1, num2);
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}`;
+    }
 }
-
 
 function checkAnswer(){
 
@@ -41,7 +49,11 @@ function incrementScore(){
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
